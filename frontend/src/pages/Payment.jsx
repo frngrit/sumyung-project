99f92b539e7cad34a659d4cb41ea571093b975e7
frngrit <frank.12876@hotmail.com>
2022-04-15 18:42:53 +0700

    make payment page

--- frontend/src/pages/Payment.jsx
+++ frontend/src/pages/Payment.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 function Payment() {
+
+  const navigate = useNavigate()
+
+  const usernameCheck = 'firstmeet1709'
+  const [username, setUser] = useState("");
+
+  useEffect(() => {
+    setUser(prompt("enter"));
+  }, []);
+
+  if (usernameCheck !== username){
+    return(<div>Not authorized</div>)
+  }
+
   return (
     <div>Payment</div>
   )
